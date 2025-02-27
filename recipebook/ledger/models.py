c3bcbcdf6816d64f0a30202554e8eb0aad3c826a
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-02-27 16:13:27 +0800

    removed db.sqlite from database and included it in git ignore. added Ingredient, Recipe, RecipeIngredient to models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    #ingredients = recipeingredient.ingrendient
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    #recipe = recipeingredient.recipe
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=10)
+    ingredient = models.ForeignKey(
+        Ingredient, 
+        on_delete=models.CASCADE,
+        related_name="recipe"
+    )
+    recipe = models.ForeignKey(
+        Recipe, 
+        on_delete=models.CASCADE,
+        related_name="ingredients"
+    )
+    
+
