4d4caf8fb4fce39899fa39adc890706f5d9e3076
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-02-27 19:34:31 +0800

    updated urls.py and views.py
    updated so that routing and rendering is FBV. page contents are first queried from the database and then sent as context to be rendered

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import recipes_list_url, recipe_1_url, recipe_2_url
+from .views import recipes_list_url, recipe
 urlpatterns = [
     path('recipes/list/', recipes_list_url, name='recipes_list_url'),
-    path('recipe/1', recipe_1_url, name='recipe_1_url'),
-    path('recipe/2', recipe_2_url, name='recipe_2_url'),
+    path('recipe/<int:id>', recipe, name='recipe'),
 ]
 # This might be needed, depending on your Django version
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,135 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Recipe, Ingredient, RecipeIngredient
+# Create your views here.
 
-recipes_list = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
-recipe_1 = {
-    "name": "Recipe 1",
-    "ingredients": [
-        {
-            "name": "tomato",
-            "quantity": "3pcs"
-        },
-        {
-            "name": "onion",
-            "quantity": "1pc"
-        },
-        {
-            "name": "pork",
-            "quantity": "1kg"
-        },
-        {
-            "name": "water",
-            "quantity": "1L"
-        },
-        {
-            "name": "sinigang mix",
-            "quantity": "1 packet"
-        }
-    ],
-    "link": "/recipe/1"
-}
-recipe_2 = {
-    "name": "Recipe 2",
-    "ingredients": [
-        {
-            "name": "garlic",
-            "quantity": "1 head"
-        },
-        {
-            "name": "onion",
-            "quantity": "1pc"
-        },
-        {
-            "name": "vinegar",
-            "quantity": "1/2cup"
-        },
-        {
-            "name": "water",
-            "quantity": "1 cup"
-        },
-        {
-            "name": "salt",
-            "quantity": "1 tablespoon"
-        },
-        {
-            "name": "whole black peppers",
-            "quantity": "1 tablespoon"
-        },
-        {
-            "name": "pork",
-            "quantity": "1 kilo"
-        }
-    ],
-    "link": "/recipe/2"
-}
+
+
 def recipes_list_url(request):
-    return render(request, "recipes_list.html", recipes_list)
-def recipe_1_url(request):
-    return render(request, "recipe.html", recipe_1)
-def recipe_2_url(request):
-    return render(request, "recipe.html", recipe_2)
+
+    recipes = Recipe.objects.all()
+    ctx = { "recipes": recipes }
+    return render(request, "recipes_list.html", ctx)
+
+
+def recipe(request,id):
+
+    recipe = Recipe.objects.get(id=id)
+    ingredients = Ingredient.objects.filter(recipe__recipe__name=recipe.name)
+    print(ingredients)
+
+    ctx = {
+        'recipe': recipe,
+        'ingredients': ingredients
+    }
+    
+    return render(request, "recipe.html",ctx)
