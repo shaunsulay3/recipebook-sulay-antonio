8343a7898662fef492fa5d23a3f5f8f9967048df
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-03-10 21:43:24 +0800

    added user to models and new migration, made admin page for users

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Recipe, RecipeIngredient, Ingredient, Profile
 
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
@@ -9,5 +19,11 @@ class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [RecipeIngredientInline,]
 
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
 
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     def get_absolute_url(self):
         return( reverse('ledger:recipe', args=[str(self.id)]))
+    
     def __str__(self):
         return self.name
     
