4f7dc3b02bdbbc1bd34a554d4dcad7bdcdee07ca
shaunsulay3 <antonio.sulay@student.ateneo.edu>
2025-03-10 21:56:23 +0800

    updated views to restrict access to logged out users, added log in log out routes, log in html, and auto-redirect to log in in settings

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Recipe, Ingredient
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,7 +11,7 @@ def recipes_list(request):
     ctx = { "recipes": recipes }
     return render(request, "recipes_list.html", ctx)
 
-
+@login_required
 def recipe(request,id):
 
     recipe = Recipe.objects.get(id=id)
